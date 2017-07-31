392b3b4538f0d9945c0ef09c32ca1f8a57f13a2e
Mohammed Mahdy <mohamed.mahdy@sphinxinfotech.net>
2017-07-31 14:47:41 +0200

    047 adding contents to recipe component

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
@@ -7,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeListComponent implements OnInit {
 
-  recipes = [];
+  recipes: Recipe[] = [];
 
-  constructor() { }
+  constructor() {
+      this.recipes = [
+          new Recipe('A test recipe', 'This is simply a test', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg')
+      ];
+  }
 
   ngOnInit() {
   }
